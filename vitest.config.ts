3e7c7b071ae63a1c118e831fd854a3ca352daa82
Jamius Siam <j@sii.am>
2026-06-09 23:19:39 +0600

    Excluded type definition files from coverage reporting in Vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       include: ["src/**/*.ts"],
+      exclude: ["src/@types/**/*.ts"],
       reporter: ["text", "html"],
     },
   },
