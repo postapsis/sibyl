9f43f03eb1a119c0ff24ea60fc1954a8aeab46ff
Jamius Siam <j@sii.am>
2026-06-12 15:17:19 +0600

    Updated Codecov action to v5 and added slug configuration Added `lcov` reporter to test coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       provider: "v8",
       include: ["src/**/*.ts"],
       exclude: ["src/@types/**/*.ts", "src/exit.ts"],
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "lcov"],
     },
   },
 });
